b38595dae3d8ad703f990eb50c78de7cef956565
BayuAlFatah <bayualfatahsetiawan5@gmail.com>
2023-01-03 18:08:48 +0700

    menambahkan sout menggunakan github

--- coba.java
+++ coba.java
@@ -2,5 +2,6 @@ class coba{
     public static void main(String[] args) {
         
         System.out.println("Hello word");
+        System.out.println("ini adalah percobaan menggunakan github");
     }
 }
